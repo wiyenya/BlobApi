bb1cd6c61cf1a0a9da21cf7fa249945a32fd8030
wiyenya <kategrigoreva2003@gmail.com>
2023-09-22 17:52:54 +0300

    Create files, add a function to the main.go file

--- internal/service/main.go
+++ internal/service/main.go
@@ -9,6 +9,9 @@ import (
 	"gitlab.com/distributed_lab/kit/copus/types"
 	"gitlab.com/distributed_lab/logan/v3"
 	"gitlab.com/distributed_lab/logan/v3/errors"
+
+	"gitlab.com/tokend/api/internal/api/handlers"
+    "gitlab.com/tokend/api/internal/api/middlewares"
 )
 
 type service struct {
@@ -40,4 +43,24 @@ func Run(cfg config.Config) {
 	if err := newService(cfg).run(); err != nil {
 		panic(err)
 	}
+}
+
+func BlobsRouter(entry *logan.Entry) chi.Router {
+    r := chi.NewRouter()
+
+    r.Use(
+        ape.RecoverMiddleware(entry),
+        middlewares.Logger(entry, 300*time.Millisecond),
+    )
+
+	// blobs
+    r.Route("/integrations/BlobApi", func(r chi.Router) {
+        r.Post("/", handlers.CreateBlob)
+        r.Get("/", handlers.GetBlobList)         // Получение списка блобов
+        r.Get("/{blobID}", handlers.GetBlob)    // Получение блоба по ID
+        r.Delete("/{blobID}", handlers.DeleteBlob) // Удаление блоба по ID
+    })
+
+
+    return r
 }
